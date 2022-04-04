e15b31fd03e43b4fa0168168bf5d95fbbe9e5cb2
Weslley Nascimento <weslley.nascimento@avitaseg.com.br>
2022-04-04 18:24:33 -0300

    criando notas e alterando estado para renderizar a pagina

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import './assets/index.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
   criarNota(titulo, texto) {
-    console.log(titulo + texto);
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {notas: novoArrayNotas};
+    this.setState(novoEstado);
   }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
