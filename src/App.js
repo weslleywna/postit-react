e30799d557027fada9a22e9eb30f5eb86ba01f8b
Weslley Nascimento <weslley.nascimento@avitaseg.com.br>
2022-04-04 17:55:43 -0300

    criando propriedade para criarNota

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import './assets/App.css';
 import './assets/index.css';
 
 class App extends Component {
+
+  criarNota(titulo, texto) {
+    console.log(titulo + texto);
+  }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
+        <FormularioCadastro criarNota={this.criarNota} />
         <ListaDeNotas />
       </section>
     );
