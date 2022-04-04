16d4d8d52011b33a6133a4cf18d9bcbd3f08514e
Weslley Nascimento <weslley.nascimento@avitaseg.com.br>
2022-04-04 15:57:51 -0300

    criando construtor fazendo o bind do titulo do formulario para o atributo da classe

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,35 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  render() {
-    return (
-      <form className="form-cadastro ">
-        <input
-          type="text"
-          placeholder="Título"
-          className="form-cadastro_input"
-        />
-        <textarea
-          rows={15}
-          placeholder="Escreva sua nota..."
-          className="form-cadastro_input"
-        />
-        <button className="form-cadastro_input form-cadastro_submit">
-          Criar Nota
-        </button>
-      </form>
-    );
-  }
+
+    constructor() {
+        super();
+        this.titulo = "";
+    }
+
+    handleMudancaTitulo(evento) {
+        this.titulo = evento.target.value;
+    }
+    render() {
+        return (
+            <form className="form-cadastro">
+                <input
+                    type="text"
+                    placeholder="Título"
+                    className="form-cadastro_input"
+                    onChange={this.handleMudancaTitulo.bind(this)}
+                />
+                <textarea
+                    rows={15}
+                    placeholder="Escreva sua nota..."
+                    className="form-cadastro_input"
+                />
+                <button className="form-cadastro_input form-cadastro_submit">
+                    Criar Nota
+                </button>
+            </form>
+        );
+    }
 }
 
 export default FormularioCadastro;
